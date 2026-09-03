92fbadf3fb6a415d878de45945aa78fcf6546de6
Pedro Suzuki <seu-email-do-github@exemplo.com>
2026-09-03 10:54:30 -0300

    Removendo arquivos desnecessários do repositorio

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Project root directory
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Input and Output Configuration
-ARQUIVO_INPUT = os.path.join(BASE_DIR, 'TesteOTInput.xlsx')
-ABA_EXCEL = 'Revisao05-08-2026'
-COLUNA_RETORNO = 'N'
