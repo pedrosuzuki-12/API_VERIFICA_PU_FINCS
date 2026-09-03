5c2660eb4fa232d9702f92ed09501541cbe0480c
Pedro Suzuki <seu-email-do-github@exemplo.com>
2026-09-03 15:24:07 -0300

    Bearer token na rota

--- api.py
+++ api.py
@@ -137,6 +137,19 @@ def processar_item(cod_ativo_i, data_i, cache_id, cache_lock):
 
 @app.route("/verificar-pu", methods=["POST"])
 def verificar_pu():
+    # --- INÍCIO DA VERIFICAÇÃO DE SEGURANÇA (BEARER TOKEN) ---
+    auth_header = request.headers.get("Authorization")
+    
+    if not auth_header or not auth_header.startswith("Bearer "):
+        return jsonify({"erro": "Acesso negado. Token ausente ou formato inválido (use Bearer)."}), 401
+        
+    # Pega apenas a parte do token (depois da palavra 'Bearer ')
+    token_recebido = auth_header.split(" ")[1]
+    
+    if token_recebido != TOKEN:
+        return jsonify({"erro": "Acesso negado. Token inválido."}), 401
+    # --- FIM DA VERIFICAÇÃO DE SEGURANÇA ---
+
     dados = request.get_json()
 
     if dados is None:
